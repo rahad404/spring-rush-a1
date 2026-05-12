6f482d2548b40ab654ac04b98a667462da748eeb
rahad404 <rahad.0911@gmail.com>
2026-05-12 23:24:48 +0600

    add: add post and user entity

---
+++ src/main/java/com/rahad404/spring_rush_a1/entity/Post.java
@@ -0,0 +1,41 @@
+package com.rahad404.spring_rush_a1.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Post {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(nullable = false)
+   private String authorName;
+
+   @Column(nullable = false, length = 280)
+   private String content;
+
+   @Column(nullable = false)
+   private LocalDateTime createdAt;
+
+   @Column(nullable = false)
+   @Builder.Default
+   private boolean active = true;
+}

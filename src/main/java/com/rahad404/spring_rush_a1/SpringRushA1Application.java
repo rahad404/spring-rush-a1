b27a1812d0e9b7ed644a749fdc838c88aad76b8a
rahad404 <rahad.0911@gmail.com>
2026-05-12 22:51:15 +0600

    add: initial commit

---
+++ src/main/java/com/rahad404/spring_rush_a1/SpringRushA1Application.java
@@ -0,0 +1,13 @@
+package com.rahad404.spring_rush_a1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRushA1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRushA1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahad404/spring_rush_a1/SpringRushA1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahad404.spring_rush_a1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRushA1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
